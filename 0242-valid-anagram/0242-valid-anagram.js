c35de4a256f227a8e22692aa3165ef77da00e7bc
Jay Mo <115325888+Jay-Mo-99@users.noreply.github.com>
2024-08-14 15:20:06 -0400

    Time: 124 ms (5.01%), Space: 61.6 MB (5.25%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,20 +4,20 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    let tempS = Object.assign([],s);
-    let uniqueS = [...tempS];
-    uniqueS.sort();
+    let tempS = Object.assign([],s);/**Convey string s to array tempS  */
+    let uniqueS = [...tempS];/**Change tempS to uniqueS로   */
+    tempS.sort();
 
-    let tempT = Object.assign([],t);
+    let tempT = Object.assign([],t);/**Convey string t to array tempT  */
     let uniqueT = [...tempT];
-    uniqueT.sort();
+    tempT.sort();
 
 
     console.log(uniqueS)
     console.log(uniqueT)
 
 
-    if(JSON.stringify(uniqueS) === JSON.stringify(uniqueT)){
+    if(JSON.stringify(tempS) === JSON.stringify(tempT)){
         return true;
     }else{
         return false;
