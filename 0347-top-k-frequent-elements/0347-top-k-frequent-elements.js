765e874de06ca7907858ad6f205cbf50aff01cd7
Jay Mo <115325888+Jay-Mo-99@users.noreply.github.com>
2024-08-23 13:09:39 -0400

    Time: 70 ms (54.14%), Space: 52.6 MB (83.99%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -4,25 +4,21 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-    nums.sort();
+    nums.sort();/**오름차순 정렬  */
+    /**Change the nums array to object that checks the num of elements */
     let temp = nums.reduce((accu,curr)=>{
         accu[curr] = (accu[curr]||0)+1;
         return accu;
-
     },{});
-    console.log(temp);/**temp는 Object형태로 존재 */
-    let downSorted = Object.entries(temp).sort((a,b)=>(b[1]-a[1]));/**Object의 value에 대한 내림차순 정렬 */
+    /**Sort the object by descending order of value  */
+    let downSorted = Object.entries(temp).sort((a,b)=>(b[1]-a[1]));
     let result = []
+    /**Create the array for just */
     for(let element of downSorted){
         result.push(element[0]);
     }
-
-
-    // let tempObj = Object.fromEntries(downSorted);
-    // console.log(tempObj);
-    // let result = Object.keys(tempObj);
-    
-    result = result.slice(0,k);
+    /**Cut off the key of the object*/
+    result = result.slice(0,k); /**인덱스 0번째부터 k번째까지 자르기 */
     console.log(result);
     return result;
     
