e11f5c9474a8b7419e261294e7f491bf66ddd324
Jay Mo <115325888+Jay-Mo-99@users.noreply.github.com>
2024-10-07 10:16:35 -0400

    Time: 86 ms (16.25%), Space: 54.6 MB (18.96%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,24 +4,27 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    let tempS = Object.assign([],s);/**Convey string s to array tempS  */
-    let uniqueS = [...tempS];/**Change tempS to uniqueS로   */
-    tempS.sort();
+    //Convert string to array
+    const arrayS = s.split("");
+    const arrayT = t.split("");
 
-    let tempT = Object.assign([],t);/**Convey string t to array tempT  */
-    let uniqueT = [...tempT];
-    tempT.sort();
-
-
-    console.log(uniqueS)
-    console.log(uniqueT)
-
-
-    if(JSON.stringify(tempS) === JSON.stringify(tempT)){
-        return true;
-    }else{
+    //Sort the array
+    const orderS = arrayS.sort();
+    const orderT = arrayT.sort();
+    //If the orderS and orderT is different num of array, it means they are not same, so return false
+    if(orderS.length !== orderT.length){
         return false;
-    }
+    }else{
+        //If they have same num of array
+        //Check orderS and orderT is same or not, approach the each element of array 
+        for(let i=0;i<orderT.length;i++){
+            if(orderS[i]!==orderT[i]){
+                return false;
+            }
 
+        }
+        return true;
+
+    }
 
 };
