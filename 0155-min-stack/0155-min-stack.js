e4c0dd9043311729a92d49ba414a3311c1c33f0c
Jay Mo <115325888+Jay-Mo-99@users.noreply.github.com>
2024-10-08 16:20:45 -0400

    Time: 100 ms (24.68%), Space: 58.7 MB (64.85%) - LeetHub

--- 0155-min-stack/0155-min-stack.js
+++ 0155-min-stack/0155-min-stack.js
@@ -1,36 +1,36 @@
 var MinStack = function() {
-    this.st = [];
-};
-
-/** 
- * @param {number} val
- * @return {void}
- */
-MinStack.prototype.push = function(val) {
-    let min_val = this.getMin();
-    if (min_val === null || min_val > val) {
-      min_val = val;
-    }
-    this.st.push([val, min_val]);
+  this.elements = [];
 };
 
 /**
- * @return {void}
+
+ @param {number} x
+ @return {void}
+ */
+MinStack.prototype.push = function(x) {
+  this.elements.push({
+    value: x,
+    min: this.elements.length === 0 ? x : Math.min(x, this.getMin()),
+  });
+};
+/**
+
+ @return {void}
  */
 MinStack.prototype.pop = function() {
-    this.st.pop();
+  this.elements.pop();
 };
-
 /**
- * @return {number}
+
+ @return {number}
  */
 MinStack.prototype.top = function() {
-    return this.st.length ? this.st[this.st.length - 1][0] : null;
+  return this.elements[this.elements.length - 1].value;
 };
-
 /**
- * @return {number}
+
+ @return {number}
  */
 MinStack.prototype.getMin = function() {
-    return this.st.length ? this.st[this.st.length - 1][1] : null;
+  return this.elements[this.elements.length - 1].min;
 };
