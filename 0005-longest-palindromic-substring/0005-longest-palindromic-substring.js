99fc0edbc6d47e8b88a551428aa6d96cd1d094de
Jay Mo <115325888+Jay-Mo-99@users.noreply.github.com>
2024-11-20 16:11:17 -0500

    Time: 642 ms (12.31%), Space: 58.3 MB (14.11%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -4,10 +4,14 @@
  */
 const longestPalindrome = (s) => {
   let answer = '';
+  //Check each element(character) of string 's'
   for (let i = 0; i < s.length; i++) {
+    //
     for (let j = 0; j < 2; j++) {
       let lt = i;
       let rt = i + j;
+      console.log("I am lt in j loop"+lt);
+      console.log("I am rt in j loop"+rt);
       while (s[lt] && s[lt] === s[rt]) {
         lt--;
         rt++;
