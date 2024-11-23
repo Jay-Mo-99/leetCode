59fef82d036b628ab9e042aa7d440449c59529af
Jay Mo <115325888+Jay-Mo-99@users.noreply.github.com>
2024-11-23 18:25:16 -0500

    Time: 0 ms (100%), Space: 52.3 MB (30.84%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -3,13 +3,14 @@
  * @return {string[]}
  */
 
-var fizzBuzz = function(n) {
-    let final = Array(n).fill("");
-    return final.map((_, index) => {
-        const i = index + 1;
-        const three = i % 3 === 0 ? "Fizz" : "";
-        const five = i % 5 === 0 ? "Buzz" : "";
-        const current = three.concat(five);
-        return current === "" ? i.toString() : current;
-    });
-};
+var fizzBuzz = function(n){
+    let answer = Array(n).fill("");//Create the empty array including all elements are ""
+    return answer.map((_,index)=>{
+        const i = index +1;
+        const three = i%3 === 0?"Fizz":"";
+        const five = i%5 === 0?"Buzz":"";
+        const current = three.concat(five);//
+        return current === ""?i.toString():current;
+
+    })
+}
