cd94155a185305cde81a1cf7e24a2c6465ddebab
Jay Mo <115325888+Jay-Mo-99@users.noreply.github.com>
2024-11-23 17:53:58 -0500

    Time: 0 ms (100%), Space: 52.5 MB (17.8%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -2,28 +2,18 @@
  * @param {number} n
  * @return {string[]}
  */
-var fizzBuzz = function(n) {
-    let result=[];
-    /**loop */
-    for(let i=0;i<n+1;i++){
-        /* if statement for multiple of 15 */
-        if(i==0){
-            result[i-1] = i.toString();
-        }
-        else if(i%15 ==0){
-
-            result[i-1] = "FizzBuzz"
-
-        }else if(i%5 ==0){
-
-            result[i-1] = "Buzz"
-        }else if(i%3==0){
-            result[i-1] = "Fizz"
+var fizzBuzz = function(n){
+    let answer =[];
+    for(let i=1;i<=n;i++){
+        if(i%3===0&&i%5===0){
+            answer.push("FizzBuzz");
+        }else if(i%3===0){
+            answer.push("Fizz");
+        }else if(i%5===0){
+            answer.push("Buzz")
         }else{
-            result[i-1] =  i.toString();
+            answer.push(i.toString());
         }
-        
     }
-
-    return result;
-};
+    return answer;
+}
