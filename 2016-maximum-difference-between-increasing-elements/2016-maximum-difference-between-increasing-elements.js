8dbcc624c55d6a95f910ce8b4a98b2926385a319
Jay Mo <115325888+Jay-Mo-99@users.noreply.github.com>
2024-11-25 17:12:30 -0500

    Time: 7 ms (45.39%), Space: 48.9 MB (79.15%) - LeetHub

---
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumDifference = function(nums) {
+    var diff=-1
+    for(let i=0;i<nums.length;i++){
+        for(let j=i+1;j<nums.length;j++){
+            if (nums[j]> nums[i]) diff=Math.max(nums[j]-nums[i],diff)
+        }
+    }
+    return diff
+};
