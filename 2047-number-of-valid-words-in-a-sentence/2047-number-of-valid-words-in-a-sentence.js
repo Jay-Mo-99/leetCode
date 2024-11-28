1dbe5832eae796145a60782da56654aed3fa52b8
Jay Mo <115325888+Jay-Mo-99@users.noreply.github.com>
2024-11-28 14:44:57 -0500

    Time: 5 ms (72.41%), Space: 52.4 MB (40%) - LeetHub

--- 2047-number-of-valid-words-in-a-sentence/2047-number-of-valid-words-in-a-sentence.js
+++ 2047-number-of-valid-words-in-a-sentence/2047-number-of-valid-words-in-a-sentence.js
@@ -3,54 +3,14 @@
  * @return {number}
  */
 var countValidWords = function(sentence) {
-    let array = sentence.split(" ");//Convert sentence to array depends on the empty space ' '.
-    let answer = 0;//The variable for saving the qualified element
-    console.log(array)
-    function containsNumber(str) {
-        return /\d/.test(str);
-    }
-    for(let i=0;i<array.length;i++){
-        if(!isNaN(array[i])||containsNumber(array[i])){
-            //숫자 안되고 ''인 space안됨 
-            console.log("Number Unvalid: "+array[i])
-        }if(array[i].includes('-')&&array[i].indexOf('-')>0&&array[i][array[i].length-1]!=='-'){
-            //여긴 숫자 이외의 모든거ㅛ 
-            //There is at most one hyphen '-'. If present, it must be surrounded by lowercase characters ("a-b" is valid, but "-ab" and "ab-" are not valid).
-            console.log("Hypen Valid: "+array[i])
-            answer++;
-        }if(array[i].includes('!')){
-            if(array[i].indexOf('!')===0||array[i].indexOf('!')===array[i][array[i].length-1]){
-                console.log("! Valid: "+array[i])
-                answer++;
-            }else{
-                console.log("! UnValid: "+array[i])
-
-            }
-
-        }if(array[i].includes('.')){
-            if(array[i].indexOf('.')===0||array[i].indexOf('.')===array[i][array[i].length-1]){
-                console.log(". Valid: "+array[i])
-                answer++;
-            }else{
-                console.log(". UnValid: "+array[i])
-
-            }
-
-        }if(array[i].includes(',')){
-            if(array[i].indexOf(',')===0||array[i].indexOf(',')===array[i][array[i].length-1]){
-                console.log(", Valid: "+array[i])
-                answer++;
-            }else{
-                console.log(", UnValid: "+array[i])
-
-            }
-
-        }else{
-            console.log("Valid: "+array[i])
-            answer++;
-        }
-    }
-
-    return answer;
-
+    let list = sentence.split(' ')
+    let filtered = list.filter(s => {
+        if (/\d/.test(s) || s === '') return false   //removes anything with numbers or is blank
+        if (/^[!,.]$/.test(s)) return true          //punctuation only
+        if (/^\w+[!,.]?$/.test(s)) return true      //word + optional punctuation
+        if (/^\w+[-]?\w+[!,.]?$/.test(s)) return true //word + optional hypen + word + optional punctuation
+        return false
+    })
+    
+    return filtered.length
 };
