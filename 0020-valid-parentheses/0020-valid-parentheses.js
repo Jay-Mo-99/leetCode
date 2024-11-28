5aaed1622c7912b1bcd4718ba5b221c03ead2694
Jay Mo <115325888+Jay-Mo-99@users.noreply.github.com>
2024-11-28 16:48:06 -0500

    Time: 5 ms (36.54%), Space: 52.2 MB (12.51%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -1,22 +1,25 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
 var isValid = function(s) {
-    let stack = []; // create an empty stack to store opening brackets
-    for (let c of s) { // loop through each character in the string
-        if (c === '(' || c === '{' || c === '[') { // if the character is an opening bracket
-            stack.push(c); // push it onto the stack
-        } else { // if the character is a closing bracket
-            if (!stack.length || // if the stack is empty or 
-                (c === ')' && stack[stack.length - 1] !== '(') || // the closing bracket doesn't match the corresponding opening bracket at the top of the stack
-                (c === '}' && stack[stack.length - 1] !== '{') ||
-                (c === ']' && stack[stack.length - 1] !== '[')) {
-                return false; // the string is not valid, so return false
+        const stack = [];
+        const mapping = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        };
+
+        for (const c of s) {
+        //If the c is the open bracket, such as the value of the mapping, push to the stack
+            if (Object.values(mapping).includes(c)) {
+                stack.push(c);
+                //If not, it is the close bracket.
+            } else if (mapping.hasOwnProperty(c)) {
+            //If the stack.lengt is empty, return false and
+            // close bracket(c) is not matched with mapping's key, return ffalse
+            //
+                if (!stack.length || mapping[c] !== stack.pop()) {
+                    return false;
+                }
             }
-            stack.pop(); // otherwise, pop the opening bracket from the stack
         }
-    }
-    return !stack.length; // if the stack is empty, all opening brackets have been matched with their corresponding closing brackets,
-                          // so the string is valid, otherwise, there are unmatched opening brackets, so return false
+				//If the stack's element are poped successfully, return true
+        return stack.length === 0;    
 };
