585d576135e06e67c1fcf32ac047ae54edb703fb
Jay Mo <115325888+Jay-Mo-99@users.noreply.github.com>
2024-11-28 14:55:37 -0500

    Time: 5 ms (72.41%), Space: 52.4 MB (40%) - LeetHub

--- 2047-number-of-valid-words-in-a-sentence/2047-number-of-valid-words-in-a-sentence.js
+++ 2047-number-of-valid-words-in-a-sentence/2047-number-of-valid-words-in-a-sentence.js
@@ -2,15 +2,21 @@
  * @param {string} sentence
  * @return {number}
  */
-var countValidWords = function(sentence) {
-    let list = sentence.split(' ')
-    let filtered = list.filter(s => {
-        if (/\d/.test(s) || s === '') return false   //removes anything with numbers or is blank
-        if (/^[!,.]$/.test(s)) return true          //punctuation only
-        if (/^\w+[!,.]?$/.test(s)) return true      //word + optional punctuation
-        if (/^\w+[-]?\w+[!,.]?$/.test(s)) return true //word + optional hypen + word + optional punctuation
-        return false
+
+
+
+var countValidWords = function(sentence){
+    let list = sentence.split(' ');//Split the sentence to array depends on ' '
+    //filter method copy the element of array and accepted the funtion inside the filter 
+    let filtered = list.filter(s=>{
+        if(/\d/.test(s)||s === '')return false //removes anything with number or is blank 
+        if(/^[!,.]$/.test(s))return true// If punctuation only return true
+        if(/^\w+[!,.]?$/.test(s)) return true //word + optional punctuation
+        if(/^\w+[-]?\w+[!,.]?$/.test(s)) return true // word + optional hypen + word + optional punctuation
+
+        return false 
     })
     
     return filtered.length
-};
+
+}
