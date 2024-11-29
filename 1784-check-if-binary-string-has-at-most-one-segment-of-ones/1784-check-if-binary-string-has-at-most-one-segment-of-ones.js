c663e36bbce0f138079a9ce54f08a38729b3a79f
Jay Mo <115325888+Jay-Mo-99@users.noreply.github.com>
2024-11-29 00:24:59 -0500

    Time: 1 ms (16.22%), Space: 49 MB (39.56%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.js
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.js
@@ -3,4 +3,17 @@
  * @return {boolean}
  */
 
-var checkOnesSegment = (s) => !s.includes("01");
+var checkOnesSegment = function(s){
+    let segment = 0;
+    for (let i=0;i<s.length;i++){
+        if(s[i]=== "1" && (i===0||s[i-1]==="0")){
+            segment ++;
+        }
+
+        if(segment>1){
+            return false;
+        }
+    }
+
+    return true;
+}
