5b81db2cedc57cb78929d24697de5c58145ea334
Jay Mo <115325888+Jay-Mo-99@users.noreply.github.com>
2024-11-29 00:32:24 -0500

    Time: 0 ms (100%), Space: 49.1 MB (39.56%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.js
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.js
@@ -6,7 +6,9 @@
 var checkOnesSegment = function(s){
     let segment = 0;
     for (let i=0;i<s.length;i++){
-        if(s[i]=== "1" && (i===0||s[i-1]==="0")){
+        //만약에 i가 0이라서 첫번째 순서면 일단 무조건 segement를 증가시키고
+        //다음 루프로 s[i] 를 살피도록 한다 
+        if(s[i]=== "1" && (i===0 ||s[i-1]==="0")){
             segment ++;
         }
 
