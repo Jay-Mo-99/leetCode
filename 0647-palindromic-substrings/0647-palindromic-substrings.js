34bda56da82fcebab0a49fcfaa97df72180ff3e4
Jay Mo <115325888+Jay-Mo-99@users.noreply.github.com>
2024-12-05 18:43:48 -0500

    Time: 15 ms (59.18%), Space: 50.7 MB (50.06%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.js
+++ 0647-palindromic-substrings/0647-palindromic-substrings.js
@@ -4,16 +4,21 @@
  */
 var countSubstrings = function(s) {
     let count = 0;
-    
-    const bubbleFromCenter = (left, right) => {
-        while(left >= 0 && right < s.length && s[left] === s[right]) {
-            count++; left--; right++;
+
+    function expandAroundCenter(left,right){
+        while(left>=0&&right<s.length&&s[left]===s[right]){
+            count++;
+            left--;
+            right++;
         }
     }
     
-    for(let i = 0; i < s.length; i++) {
-        bubbleFromCenter(i, i);
-        bubbleFromCenter(i, i+1);
-    }
+    for (let i = 0; i < s.length; i++) {
+        // 홀수 길이 팰린드롬
+        expandAroundCenter(i, i);
+        // 짝수 길이 팰린드롬
+        expandAroundCenter(i, i + 1);
+    }    
+
     return count;
 };
