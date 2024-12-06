47d61aaf1a2e3e7efb0b1f619c60bc24fbc5fc0c
Jay Mo <115325888+Jay-Mo-99@users.noreply.github.com>
2024-12-06 17:02:15 -0500

    Time: 4 ms (8.63%), Space: 50.3 MB (35.33%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.js
+++ 0048-rotate-image/0048-rotate-image.js
@@ -3,32 +3,25 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var rotate = function(matrix) {
-    let l = 0, r = 0, d = 0, temp = 0;
-    while(l < matrix.length){
-        if(r < matrix.length){
-            temp = matrix[l][r];
-            matrix[l][r] = matrix[r][l];;
-            matrix[r][l] = temp;
-            r+=1;
+    let row = matrix.length; //Save the row: The element num of total m
+    let col = matrix[0].length;//Save the length num of the each element 
+    let temp =[];
+    let answer = [];
+
+    for(let j=0;j<col;j++){
+        for(let i =row-1;i>=0;i--){
+            temp.push(matrix[i][j]);     
         }
-        else{
-            l+=1;
-            r = l;
+        console.log("After i loop temp : "+temp);
+        answer.push([...temp]);
+        temp = [];
+    }
+//You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+//Don't use new array, Edit the original matrix and return it 
+    for(let k =0;k<row;k++){
+        for(let l =0;l<col;l++){
+            matrix[k][l] = answer[k][l]
         }
     }
-    r = matrix.length - 1;
-    l = 0;
-    while(l <= r){
-        if(d < matrix.length){
-            temp = matrix[d][l];
-            matrix[d][l] = matrix[d][r];
-            matrix[d][r] = temp;
-            d+=1;
-        }
-        else{
-            d = 0;
-            l+=1;
-            r-=1;
-        }
-    }
+    return matrix;
 };
