00baf485d9d89c55c5c25e655830863e38a1864f
Jay Mo <115325888+Jay-Mo-99@users.noreply.github.com>
2024-12-06 15:41:44 -0500

    Time: 0 ms (100%), Space: 51.9 MB (44.29%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.js
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.js
@@ -3,19 +3,21 @@
  * @return {number[]}
  */
 var luckyNumbers = function(matrix) {
-     // Loop through each row of the matrix
-    for(let i =0;i<matrix.length;i++){
-        let row = matrix[i]//Current row
-        let minRow = Math.min(...row)// Find the minimum value in the current row
-        let index = row.indexOf(minRow); //Get the column index of the minimum value
-        //matrix every can chech the all element is accepted the all criteria
-        //every can check all element is satisfied the all critera at the same time
-        if(matrix.every(elem=> elem[index]<=minRow)){
-        // Example: In [[3, 6], [7, 1], [5, 2], [4, 8]], "3" is minRow in row 0 (index 0),
-        //In this case, every method check elem[0] <= 3,(3<=3,7<=3,5<=3,4<=3) → Condition fails
-            return [minRow]
-        } 
-    }
-    return [];
+    // Loop through each row of the matrix
+   for(let i =0;i<matrix.length;i++){
+       let row = matrix[i]//Current row
+       let minRow = Math.min(...row)// Find the minimum value in the current row
+       let index = row.indexOf(minRow); //Get the column index of the minimum value
+       //matrix every can chech the all element is accepted the all criteria
+       //every can check all element is satisfied the all critera at the same time
+       if(matrix.every(elem=> elem[index]<=minRow)){
+       // Example: In [[3, 6], [7, 1], [5, 2], [4, 8]], "3" is minRow in row 0 (index 0),
+       //In this case, every method check elem[0] <= 3,(3<=3,7<=3,5<=3,4<=3) → Condition fails
+       //Example: In [[3,7,8],[9,11,13],[15,16,17]], "3" is minRow in row 0(index 0),
+       //In this case, every method check elem[0]<=3,(3<=9 ->false)
+           return [minRow]
+       } 
+   }
+   return [];
 
 };
