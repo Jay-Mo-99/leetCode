cc7d5b2437ba2bae4fc3d143e2b463f348d3a485
Jay Mo <115325888+Jay-Mo-99@users.noreply.github.com>
2024-12-06 15:20:03 -0500

    Time: 1 ms (87.2%), Space: 52 MB (44.29%) - LeetHub

---
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var luckyNumbers = function(matrix) {
+     // Loop through each row of the matrix
+    for(let i =0;i<matrix.length;i++){
+        let row = matrix[i]//Current row
+        let minRow = Math.min(...row)// Find the minimum value in the current row
+        let index = row.indexOf(minRow); //Get the column index of the minimum value
+        //matrix every can chech the all element is accepted the all criteria
+        //every can check all element is satisfied the all critera at the same time
+        if(matrix.every(elem=> elem[index]<=minRow)){
+        // Example: In [[3, 6], [7, 1], [5, 2], [4, 8]], "3" is minRow in row 0 (index 0),
+        //In this case, every method check elem[0] <= 3,(3<=3,7<=3,5<=3,4<=3) → Condition fails
+            return [minRow]
+        } 
+    }
+    return [];
+
+};
