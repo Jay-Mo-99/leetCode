4c9d24085e2bc8dc2dbe6d646f3bcf8c1497743d
Jay Mo <115325888+Jay-Mo-99@users.noreply.github.com>
2024-12-17 18:50:34 -0500

    Time: 0 ms (100%), Space: 12.5 MB (7.03%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,18 +1,18 @@
+import math
+
 class Solution(object):
     def climbStairs(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        if n==1:
-            return n
-        if n == 2:
-            return 2
-        seen = [1, 2]
-        i = 3
-        while i < n+1:
-            seen.append(seen[i-3]+seen[i-2])
-            i+=1
-
-
-        return seen[-1]
+        if n <= 1:
+            return 1
+        
+        dp = [-1] * (n + 1)
+        return self.climbStairsHelper(n, dp)
+    
+    def climbStairsHelper(self, n, dp):
+        if n <= 1:
+            return 1
+        if dp[n] != -1:
+            return dp[n]
+        
+        dp[n] = self.climbStairsHelper(n - 1, dp) + self.climbStairsHelper(n - 2, dp)
+        return dp[n]
