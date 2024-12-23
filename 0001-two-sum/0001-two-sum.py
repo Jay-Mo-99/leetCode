c11eb50c1e36c84a6b3c44546714c8ef909d61e6
Jay Mo <115325888+Jay-Mo-99@users.noreply.github.com>
2024-12-23 18:24:42 -0500

    Time: 11 ms (51.18%), Space: 13.6 MB (7.24%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,29 +1,13 @@
 class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-                # 원래 인덱스를 보존하기 위해 nums의 각 요소와 인덱스를 함께 저장
-        nums_with_index = [(num, idx) for idx, num in enumerate(nums)]
-        # 숫자를 기준으로 정렬
-        nums_with_index.sort(key=lambda x: x[0])
-
-        # 투 포인터 초기화
-        low, high = 0, len(nums_with_index) - 1
-
-        while low < high:
-            # 두 숫자의 합 계산
-            total = nums_with_index[low][0] + nums_with_index[high][0]
-            if total == target:
-                # 합이 target과 같으면 인덱스 반환 (원래 인덱스를 반환해야 함)
-                return [nums_with_index[low][1], nums_with_index[high][1]]
-            elif total < target:
-                # 합이 작으면 low 증가
-                low += 1
+    def twoSum(self, nums, target): 
+        nums_with_index = [(num,i) for i,num in enumerate(nums)]
+        nums_with_index.sort()
+        left,right = 0,len(nums)-1
+        while left < right:
+            if nums_with_index[left][0] + nums_with_index[right][0] == target:
+                return [nums_with_index[left][1],nums_with_index[right][1]]
+            elif nums_with_index[left][0] + nums_with_index[right][0] < target:
+                left +=1
             else:
-                # 합이 크면 high 감소
-                high -= 1
-
-        return []  # 답이 없는 경우 (문제에서 보장되지만 안전장치로 추가)
+                right -=1
+        return []
