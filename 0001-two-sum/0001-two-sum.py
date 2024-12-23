e5895bbe8c6e67b995f9c758991b17114dcb2be0
Jay Mo <115325888+Jay-Mo-99@users.noreply.github.com>
2024-12-23 18:26:29 -0500

    Time: 11 ms (51.18%), Space: 14.1 MB (7.24%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,13 +1,26 @@
 class Solution(object):
-    def twoSum(self, nums, target): 
-        nums_with_index = [(num,i) for i,num in enumerate(nums)]
-        nums_with_index.sort()
-        left,right = 0,len(nums)-1
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # nums의 각 요소와 인덱스를 저장한 뒤 숫자를 기준으로 정렬
+        nums_with_index = [(num, idx) for idx, num in enumerate(nums)]
+        nums_with_index.sort(key=lambda x: x[0])  # 숫자를 기준으로 정렬
+
+        # 투 포인터 초기화
+        left = 0
+        right = len(nums_with_index) - 1
+
         while left < right:
-            if nums_with_index[left][0] + nums_with_index[right][0] == target:
-                return [nums_with_index[left][1],nums_with_index[right][1]]
-            elif nums_with_index[left][0] + nums_with_index[right][0] < target:
-                left +=1
+            current_sum = nums_with_index[left][0] + nums_with_index[right][0]
+            if current_sum == target:
+                # 원래 배열의 인덱스를 반환 (정렬 이전의 인덱스)
+                return sorted([nums_with_index[left][1], nums_with_index[right][1]])
+            elif current_sum < target:
+                left += 1  # 합이 작으면 left를 오른쪽으로 이동
             else:
-                right -=1
-        return []
+                right -= 1  # 합이 크면 right를 왼쪽으로 이동
+
+        return []  # 조건을 만족하는 두 숫자가 없을 경우 빈 리스트 반환
