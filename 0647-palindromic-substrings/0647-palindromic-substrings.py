011ce17f8e9d4c383b4d499513f1c735340415e5
Jay Mo <115325888+Jay-Mo-99@users.noreply.github.com>
2025-01-02 18:24:24 -0500

    Time: 947 ms (16.83%), Space: 106.3 MB (6.1%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,5 +1,9 @@
-class Solution:
-    def countSubstrings(self, s: str) -> int:
+class Solution(object):
+    def countSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
         dp = {}
 
         for end in range(len(s)):
