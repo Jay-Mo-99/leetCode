e9a34d951182c911be695f8b9982c91043e1a536
Jay Mo <115325888+Jay-Mo-99@users.noreply.github.com>
2026-04-23 20:14:24 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,17 +1,7 @@
 class Solution:
-    def isAnagram(self, s: str, t: str):
-                #If the length of sorted strings are not same, 
-        #Return False
-        if(len(s)!=len(t)):
-            return False
-        #Sort s and t
-        sorted_s="".join(sorted(s))
-        sorted_t="".join(sorted(t))
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
+        for c in set(s):
+            if s.count(c) != t.count(c): return False
 
-        #Compare each character of strings
-        for i in range(len(sorted_s)):
-            #If s[i] is not same with t[i], return false
-            if(sorted_s[i] != sorted_t[i]):
-                return False
-        return True #Otherwise, return true
-        
+        return True
